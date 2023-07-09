336269d75af4f652c5a3087415f11536f65e865e
Suman Rayamajhi <sams.rayamajhi@gmail.com>
2023-07-09 23:20:46 +0200

    "add new CV and add some new skills"

--- client/src/Components/About/About.jsx
+++ client/src/Components/About/About.jsx
@@ -6,6 +6,7 @@ import {
   FaNodeJs,
   FaBootstrap,
 } from "react-icons/fa";
+import { SiTailwindcss, SiExpress, SiMysql } from "react-icons/si";
 import { DiJavascript, DiPostgresql } from "react-icons/di";
 import { FiFigma } from "react-icons/fi";
 import "./About.css";
@@ -18,15 +19,14 @@ function About() {
           <div className="col__2">
             <img src="/img/suman-3-1.png" alt="img" className="about__img" />
           </div>
-          <div className="col__2">
+          <div className="col__2 about__2">
             <h1 className="about__heading">About Me</h1>
             <div className="about__meta">
               <p className="about__text p__color">
-                An energetic and ambitious, new web-developer with proven
-                ability of a strong ethics and passion for web design, is
-                excited to use talent and grow skills through hard work and
-                motivational aspiration in new dynamic and progressive
-                companies.
+                An energetic and ambitious, web-developer with proven ability of
+                a strong ethics and passion for web design, is excited to use
+                talent and grow skills through hard work and motivational
+                aspiration in new dynamic and progressive companies.
               </p>
               <div className="About__skills">
                 <h2 className="About__skills__title">Skills</h2>
@@ -44,19 +44,39 @@ function About() {
                     <div className="About__skills__list">
                       <FaReact /> <span> React</span>
                     </div>
-                  </div>
-                  <div>
+                    <div className="About__skills__list">
+                      <img
+                        className="next-logo"
+                        src="nextjs-logo.png"
+                        alt="nextjs logo"
+                      />{" "}
+                      <span> Next.js</span>
+                    </div>
                     <div className="About__skills__list">
                       <FaNodeJs />
                       <span> Node</span>
+                    </div>
+                  </div>
+                  <div>
+                    <div className="About__skills__list">
+                      <SiExpress />
+                      <span> Express</span>
                     </div>
                     <div className="About__skills__list">
                       <DiPostgresql />
                       <span> PostgreSQL</span>
                     </div>
                     <div className="About__skills__list">
+                      <SiMysql />
+                      <span> MySQL</span>
+                    </div>
+                    <div className="About__skills__list">
                       <FaBootstrap />
                       <span> Bootstrap</span>
+                    </div>
+                    <div className="About__skills__list">
+                      <SiTailwindcss />
+                      <span> Tailwind CSS</span>
                     </div>
                     <div className="About__skills__list">
                       <FiFigma />
@@ -64,17 +84,15 @@ function About() {
                     </div>
                   </div>
                 </div>
-
-                <div className="about__button d__flex align__items__center">
-                  <a
-                    href="SumanRayamajhi_CV.pdf"
-                    download={"SumanRayamajhi_CV.pdf"}
-                  >
-                    {" "}
-                    <button className="about btn pointer">Download CV</button>
-                  </a>
-                </div>
               </div>
+            </div>
+            <div className="about__button d__flex align__items__center">
+              <a
+                href="SumanRayamajhi_CV.pdf"
+                download={"SumanRayamajhi_CV.pdf"}
+              >
+                <button className="about btn pointer">Download CV</button>
+              </a>
             </div>
           </div>
         </div>
